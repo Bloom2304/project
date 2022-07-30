e78ce328b3730dc84c82f3ae1228562f13764c15
Slava <example@gmail.com>
2022-07-30 21:26:54 +0300

    firstLessonDone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log (2*4 === +'8');
+/*jshint esversion: 6 */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('Как бы вы оценили его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('Как бы вы оценили его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
